11553594b434df3cab3beb0e7ab25fb12ec116c3
SAKSHI GUPTA <sakshi.1208brt@gmail.com>
2026-04-12 01:17:56 +0530

    Optimal solution of Majority element

--- Arrays/Easy/4_remove_duplicates.cpp
+++ Arrays/Easy/4_remove_duplicates.cpp
@@ -6,29 +6,29 @@ using namespace std;
 //brute
 //TC = O(NlogN)+O(N)
 //SC = O(N)
-int remove_duplicate(vector<int>& v){
-    set<int> s;
-    for(auto it:v){ // Set insertion takes O(NlogN) T.C
-        s.insert(it);
-    }
-    int index = 0;
-    for(auto it: s){
-        v[index] = it;
-        index++;
-    }
-    return index;
-}
-//optimal
 // int remove_duplicate(vector<int>& v){
-//     int index = 0;
-//     for (int j = 1; j < v.size(); j++){
-//         if(v[j]!=v[index]){
-//             v[index + 1] = v[j];
-//             index++;
-//         }
+//     set<int> s;
+//     for(auto it:v){ // Set insertion takes O(NlogN) T.C
+//         s.insert(it);
 //     }
-//     return index + 1;
+//     int index = 0;
+//     for(auto it: s){
+//         v[index] = it;
+//         index++;
+//     }
+//     return index;
 // }
+//optimal
+int remove_duplicate(vector<int>& v){
+    int index = 0;
+    for (int j = 1; j < v.size(); j++){
+        if(v[j]!=v[index]){
+            v[index + 1] = v[j];
+            index++;
+        }
+    }
+    return index + 1;
+}
 int main(){
     int n;
     cin >> n;
--- Arrays/Medium/3_find_majority_element.cpp
+++ Arrays/Medium/3_find_majority_element.cpp
@@ -39,8 +39,32 @@ using namespace std;
 //Optimal Approach
 //Using Boyer-Moore Voting Algorithm
 // TC = O(N) and SC = O(1)
-int majority_element(vector<int>& arr){
-
+int majority_element(vector<int>& arr) {
+    int el;
+    int cnt =0;
+    int n = arr.size();
+    for(int i=0 ; i<n ; i++){
+        if(cnt == 0){
+            cnt =1;
+            el = arr[i];
+        }
+        else if(el == arr[i]){
+            cnt++;
+        }
+        else{
+            cnt--;
+        }
+    }
+    int cnt1 = 0;
+    for(int i=0 ; i<n ; i++){
+        if(arr[i] == el){
+            cnt1++;
+        }
+    }
+    if(cnt1 > n/2){
+        return el;
+    }
+    return -1;
 }
 int main(){
     // Your code goes here
