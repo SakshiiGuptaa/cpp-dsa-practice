28418d75de435cb4411aa508617fd4ffec521154
SAKSHI GUPTA <sakshi.1208brt@gmail.com>
2026-04-16 01:02:20 +0530

    Optimal Solution of Max subarray sum using Kadane's algo

--- Arrays/Medium/4_kadane_algo.cpp
+++ Arrays/Medium/4_kadane_algo.cpp
@@ -22,14 +22,43 @@ using namespace std;
 //Better Approach
 //TC = O(N^2) and SC = O(1)
 
+// int find_max_subarray_sum(vector<int>& arr){
+//     int n = arr.size();
+//     int max_sum = INT_MIN;
+//     for(int i = 0; i < n; i++){
+//         int sum = 0;
+//         for(int j = i; j < n; j++){
+//             sum += arr[j];
+//             max_sum = max(max_sum, sum);
+//         }
+//     }
+//     return max_sum;
+// }
+
+//Optimal Approach
+//Using Kadane's Algorithm
+//TC = O(N) and SC = O(1)
+
 int find_max_subarray_sum(vector<int>& arr){
     int n = arr.size();
     int max_sum = INT_MIN;
+    int sum = 0;
+    int start, ansStart = 0, ansEnd = 0;
     for(int i = 0; i < n; i++){
-        int sum = 0;
-        for(int j = i; j < n; j++){
-            sum += arr[j];
-            max_sum = max(max_sum, sum);
+
+        //for printing the subarray with maximum sum
+        // if(sum ==0){
+        //     start = i;
+        // }
+        sum += arr[i];
+        if(sum > max_sum){
+            max_sum = sum;
+            //for printing the subarray with maximum sum
+            // ansStart = start;
+            // ansEnd = i;
+        }
+        if(sum < 0){
+            sum = 0;
         }
     }
     return max_sum;
